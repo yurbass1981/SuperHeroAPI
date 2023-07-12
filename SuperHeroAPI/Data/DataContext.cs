3fc48051b0248abb9ee94fd11ee3d5f7ddfa3127
Yuri Ilyushin <yurbass1@rambler.ru>
2023-07-12 13:16:52 +0300

    Implemented the DataContext

---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿global using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("");
+        }
+    }
+}
