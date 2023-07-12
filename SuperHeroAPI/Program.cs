922dfc4437c0472a5a5f46a0777e5b4c8e83169b
Yuri Ilyushin <yurbass1@rambler.ru>
2023-07-12 12:45:03 +0300

    Registered SuperHeroService

--- SuperHeroAPI/Program.cs
+++ SuperHeroAPI/Program.cs
@@ -1,4 +1,5 @@
 global using SuperHeroAPI.Models;
+using SuperHeroAPI.Services.SuperHeroService;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
+
 
 var app = builder.Build();
 
